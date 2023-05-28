a32e555028eb0dfab7cdd54d2b7f01780e4530b3
Koushul <kor11@pitt.edu>
2023-05-28 10:43:25 -0400

    loss fucntions

--- modules/losses.py
+++ modules/losses.py
@@ -2,7 +2,6 @@ from matplotlib import pyplot as plt
 import torch
 import torch.nn.functional as F
 
-
 class LossFunctions:
     
     def __init__(self, reconWeight, ridgePi, y_true_raw):
@@ -15,36 +14,12 @@ class LossFunctions:
         self.zinb = []
         self.loss = []
         
-      
-    def plot(self):
-        fig, axs = plt.subplots(1, 4, figsize=(20, 5))
-        axs[0].plot(self.penalties)
-        axs[0].set_title('Penalties')
-        axs[1].plot(self.kl)
-        axs[1].set_title('KL')
-        axs[2].plot(self.ce)
-        axs[2].set_title('CE')
-        axs[3].plot(self.zinb)
-        axs[3].set_title('ZINB')
-        plt.show()
-        
-        
-    def compute(self, varz, reg:bool=True):        
-        loss_kl = self.KL(varz.mu, varz.logvar, varz.train_nodes_idx) ## latent dist vs prior
-        loss_x = self.ZINB(varz.features_recon, varz.feature, varz.train_nodes_idx) ## gene reconstruction 
-        loss_a = self.CE(varz.adj_recon, varz.adj_label, varz.pos_weight, varz.norm, varz.train_nodes_idx) ## adj reconstruction
-        loss = loss_kl + loss_x + loss_a
-
-        if reg: 
-            p = self.penalty(varz.z, varz.sp_dists)
-            loss = loss + p
-            self.penalties.append(float(p))
-        
-        self.loss.append(float(loss))
-        return loss
-    
-    
     def penalty(self, z, sp_dists):
+        """            
+        Args:
+        - z: tensor of shape (batch_size, latent_dim) representing the latent variables.
+        - sp_dists: tensor of shape (batch_size, batch_size) representing the shortest path distances between nodes.
+        """
         z_dists = torch.cdist(z, z, p=2)
         z_dists = torch.div(z_dists, torch.max(z_dists))
         n_items = z.size(dim=0) * z.size(dim=0)
@@ -52,25 +27,58 @@ class LossFunctions:
         return p
     
     def KL(self, mu, logvar, nodemask=None, reduction='mean'):
+        """
+        Calculates the KL divergence between the latent distribution and the prior distribution.
+        Given two probability distributions P and Q, 
+        the KL divergence from P to Q measures the amount of information lost when Q is used to approximate P.
+
+        Args:
+        - mu: tensor of shape (batch_size, latent_dim) representing the mean of the latent distribution.
+        - logvar: tensor of shape (batch_size, latent_dim) representing the log variance of the latent distribution.
+        - nodemask: tensor of shape (batch_size,) representing the mask for the nodes.
+        - reduction: string representing the reduction method for the KL divergence.
+
+        Returns:
+        - kl: tensor representing the KL divergence.
+        """
         if reduction == 'mean':
             f = torch.mean
             if nodemask is None:
                 s = mu.size()[0]
             else:
                 s = nodemask.size()[0]
+        
         elif reduction == 'sum':
             f = torch.sum
             s = 1
-        if nodemask is None:
-            kl = -(0.5 / s) * flask(torch.sum(1 + 2 * logvar - mu.pow(2) - logvar.exp().pow(2), 1))
-            return kl
-        kl = -(0.5 / s) * f(torch.sum(1 + 2 * logvar[nodemask] - mu[nodemask].pow(2) - logvar[nodemask].exp().pow(2), 1))
         
+        if nodemask is None:
+            kl = -(0.5 / s) * torch.sum(1 + 2 * logvar - mu.pow(2) - logvar.exp().pow(2), 1)
+            return kl
+        
+        kl = -(0.5 / s) * f(torch.sum(1 + 2 * logvar[nodemask] - mu[nodemask].pow(2) - logvar[nodemask].exp().pow(2), 1))
+
         self.kl.append(float(kl))
         return kl
     
     
+    
     def CE(self, preds, labels, pos_weight, norm, nodemask=None):
+        """
+        Calculates the cross-entropy loss between the predicted and true adjacency matrices.
+        The pos_weight argument is used to weight the positive class, 
+        and the norm argument is used to normalize the loss by the number of nodes in the graph.
+        
+        Args:
+        - preds: tensor of shape (batch_size, batch_size) representing the predicted adjacency matrix.
+        - labels: tensor of shape (batch_size, batch_size) representing the true adjacency matrix.
+        - pos_weight: float representing the weight of the positive class.
+        - norm: float representing the normalization factor.
+        - nodemask: tensor of shape (batch_size,) representing the mask for the nodes.
+        
+        Returns:
+        - cost: tensor representing the cross-entropy loss.
+        """
         if nodemask is None:
             cost = norm * F.binary_cross_entropy_with_logits(
                 preds, 
@@ -94,6 +102,17 @@ class LossFunctions:
 
 
     def NB(self, preds, y_true, mask, reconWeight, eps=1e-10, ifmean=True):
+        """
+        Calculates the negative binomial reconstruction loss between the predicted and true gene expression values.
+        
+        Args:
+        - preds: tuple of tensors representing the predicted gene expression values, pi, theta, and y_pred.
+        - y_true: tensor of shape (batch_size,) representing the true gene expression values.
+        - mask: tensor of shape (batch_size,) representing the mask for the nodes.
+        - reconWeight: float representing the weight of the reconstruction loss.
+        - eps: float representing the epsilon value.
+        - ifmean: boolean representing whether to calculate the mean or not.
+        """
         output, pi, theta, y_pred = preds
         nbloss1 = torch.lgamma(theta+eps) + torch.lgamma(y_true+1.0) - torch.lgamma(y_true+theta+eps)
         nbloss2 = (theta + y_true) * torch.log(1.0 + (y_pred/(theta+eps))) + (y_true * (torch.log(theta+eps) - torch.log(y_pred+eps)))
@@ -103,6 +122,15 @@ class LossFunctions:
         return nbloss
 
     def ZINB(self, preds, y_true, mask, eps=1e-10):
+        """
+        Calculates the zero-inflated negative binomial reconstruction loss between the predicted and true gene expression values.
+        
+        Args:
+        - preds: tuple of tensors representing the predicted gene expression values, pi, theta, and y_pred.
+        - y_true: tensor of shape (batch_size,) representing the true gene expression values.
+        - mask: tensor of shape (batch_size,) representing the mask for the nodes.
+        - eps: float representing the epsilon value.
+        """
         output, pi, theta, y_pred = preds
         reconWeight, ridgePi, y_true_raw = self.reconWeight, self.ridgePi, self.y_true_raw
         nb_case = self.NB(preds, y_true, mask, reconWeight, eps=1e-10, ifmean=False)- torch.log(pi+eps)
@@ -115,3 +143,27 @@ class LossFunctions:
         zloss = result*reconWeight
         self.zinb.append(float(zloss))
         return zloss
+
+
+class LossKZCP(LossFunctions):
+    
+    def compute(self, varz):        
+        loss_kl = self.KL(varz.mu, varz.logvar, varz.train_nodes_idx) ## latent dist vs prior
+        loss_x = self.ZINB(varz.features_recon, varz.feature, varz.train_nodes_idx) ## gene reconstruction 
+        loss_a = self.CE(varz.adj_recon, varz.adj_label, varz.pos_weight, varz.norm, varz.train_nodes_idx) ## adj reconstruction
+        loss_p = self.penalty(varz.z, varz.sp_dists)
+        loss = loss_kl + loss_x + loss_a + loss_p
+        self.penalties.append(float(loss_p))
+        self.loss.append(float(loss))
+        return loss
+    
+    
+    def __repr__(self):
+        return f"LossFunctions(penalty={self.penalty.__name__}, " \
+               f"KL={self.KL.__name__}, " \
+               f"CE={self.CE.__name__}, " \
+               f"ZINB={self.ZINB.__name__})"
+
+
+
+    
